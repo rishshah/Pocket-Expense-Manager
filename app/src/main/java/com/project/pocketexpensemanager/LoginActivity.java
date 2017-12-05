69dbfb721f17d33b8e1d6eaf66029d03cb2ffe15
rrriiissshhh <rishrocks17@gmail.com>
2017-12-05 13:31:35 +0530

    Drivebase removed

--- app/src/main/java/com/project/pocketexpensemanager/LoginActivity.java
+++ app/src/main/java/com/project/pocketexpensemanager/LoginActivity.java
@@ -2,65 +2,21 @@ package com.project.pocketexpensemanager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.tasks.Task;
-import com.project.pocketexpensemanager.constant.Constants;
-
-public class LoginActivity extends DriveBase {
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setProgressBar((ProgressBar) findViewById(R.id.progress_bar));
-        showProgress(false);
-        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                signIn();
-                showProgress(true);
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
             }
-        });
+        }, 1000);
     }
-
-    /**
-     * Called after the user has signed in and the Drive client has been initialized.
-     */
-    @Override
-    public void onDriveClientReady(){
-        showMessage("Sign In Successful");
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        showProgress(false);
-    }
-
-    /**
-     * Handles resolution callbacks.
-     */
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case Constants.REQUEST_CODE_SIGN_IN:
-                if (resultCode != RESULT_OK) {
-                    showMessage( String.valueOf(resultCode) + " :Sign-in failed 1");
-                    finish();
-                    return;
-                }
-                Task<GoogleSignInAccount> getAccountTask =
-                        GoogleSignIn.getSignedInAccountFromIntent(data);
-                if (getAccountTask.isSuccessful()) {
-                    initializeDriveClient(getAccountTask.getResult());
-                } else {
-                    showMessage(String.valueOf(resultCode) + " :Sign-in failed 2");
-                    finish();
-                }
-                break;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
 }
